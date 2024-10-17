5b53a8809466aa4f8e21ab6ef114b58a6258c379
eric a <e.rock.at@gmail.com>
2024-10-17 13:54:44 -0400

    added appbar, cleaned up comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const dustwallet());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class dustwallet extends StatelessWidget {
+  const dustwallet({super.key});
 
   // This widget is the root of your application.
   @override
@@ -13,69 +13,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'dust_wallet',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        //useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'dust'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -87,63 +43,114 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: // Logo on the left side
               Image.asset(
-                'assets/dust_symbol.png', // Replace with your logo image path
-                height: 40,
+                'assets/dust_symbol.png',
+                height: 48,
               ),
             ),
             // wallet on the right side
-            GestureDetector(
-              onTap: () {
-                // TODO: open up wallet connector
-              },
-              child: // Logo on the left side
-              Image.asset(
-                'assets/wallet_icon.png', // Replace with your logo image path
-                height: 40,
-              ),
-            ),
-
-            // Dropdown menu on the right side
-
+            DropdownWithGesture(),
           ],
         ),
         backgroundColor: Colors.white,
       ),
       body:
       Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DropdownWithGesture extends StatefulWidget {
+  @override
+  _DropdownWithGestureState createState() => _DropdownWithGestureState();
+}
+
+class _DropdownWithGestureState extends State<DropdownWithGesture> {
+  GlobalKey _gestureKey = GlobalKey();
+
+  void _showDropdownMenu() {
+    final RenderBox renderBox = _gestureKey.currentContext!.findRenderObject() as RenderBox;
+    final position = renderBox.localToGlobal(Offset.zero);  // Gets the position of the GestureDetector
+    final size = renderBox.size;
+
+    showMenu(
+      context: context,
+      position: RelativeRect.fromLTRB(
+        position.dx,
+        position.dy + size.height, // Positioning the dropdown just below the GestureDetector
+        position.dx + size.width,
+        position.dy,
+      ),
+      items: [
+        PopupMenuItem<String>(
+          value: 'Aleph_Zero_Vault',
+          child: Row(
+            children: [
+              Text('Aleph Zero Vault'),
+            ],
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'SubWallet',
+          child: Row(
+            children: [
+              Text('SubWallet'),
+            ],
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'Talisman',
+          child: Row(
+            children: [
+              Text('Talisman'),
+            ],
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'Polkadot',
+          child: Row(
+            children: [
+              Text('Polkadot{.js}'),
+            ],
+          ),
+        ),
+        PopupMenuItem<String>(
+          //TODO: only display this option on mobile app version, not web app
+          value: 'Nova_Wallet',
+          child: Row(
+            children: [
+              Text('Nova Wallet (app only)'),
+            ],
+          ),
+        ),
+      ],
+    ).then((value) {
+      if (value != null) {
+        // Handle the selection here (navigation or actions)
+        print('$value selected');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      key: _gestureKey,
+      onTap: _showDropdownMenu,
+      child: Container(
+        padding: EdgeInsets.all(8),
+        //color: Colors.blue,
+        child: Image.asset(
+          'assets/wallet_icon.png',
+          height: 48,
+        ),
+      ),
     );
   }
 }
